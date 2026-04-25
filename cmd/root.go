18f5d0789f2f9482fb2d9efac4e095ee345e8fbe
tumeraltunbas <tumer@adsby.co>
2026-04-25 21:33:39 +0300

    chore: initialize nomad

---
+++ cmd/root.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "nomad",
+	Short: "Nomad is a CLI tool that keeps your AI development environment consistent across every machine you work on.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Welcome to Nomad! Type 'nomad --help' to see what it can do.")
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/tumeraltunbas/nomad/cmd"
+
+func main() {
+	cmd.Execute()
+}
